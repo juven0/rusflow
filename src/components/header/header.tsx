e923c4b9d2c8f6fcdaea629e3850f89f2280ec3a
juvenO <bocquelin@gmail.com>
2024-02-19 23:13:46 +0300

    ajout des routes

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import './header.scss'
+
+function Header(): JSX.Element{
+    return(
+        <div className="Header">
+            <label htmlFor="">header</label>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,11 @@
+import Header from "../../components/header/header"
+
+function Main():JSX.Element{
+    return(
+        <div className="Main">
+            <Header/>
+        </div>
+    )
+}
+
+export default Main
