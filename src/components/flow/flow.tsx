595123147d4e2a67ca747729baad3ee1d639eb78
juvenO <bocquelin@gmail.com>
2024-02-23 00:19:37 +0300

    ajout de la composant custom

--- src/components/flow/flow.tsx
+++ src/components/flow/flow.tsx
@@ -2,13 +2,20 @@ import ReactFlow, {
     Controls,
     Background,
     Node,
-    Edge
+    Edge, 
+    MiniMap,
+    useNodesState,
+    useEdgesState,
+    Connection,
+    addEdge
   } from 'reactflow';
 
 import './flow.scss'
 import 'reactflow/dist/style.css';
+import CustomNode from './nodes/nodes'
+import { useCallback } from 'react';
 
-const nodes:Node[]= [
+const initialNodes:Node[]= [
     {
         id: '1',
         data: { label: 'Hello' },
@@ -17,20 +24,40 @@ const nodes:Node[]= [
       },
       {
         id: '2',
+        type:'custom',
         data: { label: 'World' },
         position: { x: 100, y: 100 },
       },
   ];
-  const edges:Edge[] = [{ id: '1-2', source: '1', target: '2', label: 'to the', type: 'step' }];
+
+const initialEdges:Edge[] = [{ id: '1-2', source: '1', target: '2', label: 'to the', type: 'step' }];
+
+const nodeTypes={
+    custom: CustomNode
+}
 
 function Flow():JSX.Element{
 
+    const [nodes, , onNodesChange] = useNodesState(initialNodes)
+    const [edgs, setEdgs, onEdgesChange] = useEdgesState(initialEdges)
+
+    const onConnect = useCallback(
+        (params: Edge| Connection)=>setEdgs((els)=> addEdge(params, els)), [setEdgs]
+    )
    
     return(
         <div className="Flow">
-            <ReactFlow nodes={nodes} edges={edges}>
+            <ReactFlow 
+                nodes={nodes} 
+                edges={edgs}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}    
+                onConnect={onConnect}
+                nodeTypes={nodeTypes}
+            >
                 <Background />
                 <Controls />
+                <MiniMap />
             </ReactFlow>
         </div>
     )
