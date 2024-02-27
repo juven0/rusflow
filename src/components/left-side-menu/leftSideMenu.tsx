ccd70e96236e8f6dace48f95dd2c72d7c7c0d03c
juvenO <bocquelin@gmail.com>
2024-02-27 21:37:10 +0300

    ajout des widget

--- src/components/left-side-menu/leftSideMenu.tsx
+++ src/components/left-side-menu/leftSideMenu.tsx
@@ -1,10 +1,11 @@
+import Widget from '../widget/widget'
 import './leftSideMenu.scss'
 
 
 function LeftSideMenu(): JSX.Element{
     return(
         <div className="LeftSideMenu">
-
+            <Widget/>
         </div>
     )
 }
---
+++ src/components/widget/widget.tsx
@@ -0,0 +1,18 @@
+import './widget.scss'
+
+function Widget():JSX.Element{
+    return(
+        <div className="Widget">
+            <div className="header">
+                <label htmlFor=""></label>
+                <label className='type'>button</label>
+                <label htmlFor=""></label>
+            </div>
+            <div className="info">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Widget
